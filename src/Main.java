1bcc7c0ce96c211babe3f90fcf9c9a9508748b5e
JamesyLive <jamesjrbus@gmail.com>
2023-05-16 19:40:15 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String democrat = "donkey";
+        String republican = "elephant";
+        String independant = "man";
+        String yourParty = "D";
+
+        if (yourParty.equals("D"))
+        {
+            System.out.println("You are a democratic " + democrat);
+        }
+        else if (yourParty.equals("R"))
+        {
+            System.out.println("You are a republican " + republican);
+        }
+        else if (yourParty.equals("I"))
+        {
+            System.out.println("You are an independant " + independant);
+        }
+        else
+        {
+            System.out.println("Error. Check your spelling.");
+        }
+    }
+}
